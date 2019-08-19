0dd64a3aeae5a451532301ab2da3763b37a97cf5
diegodiogenes <diediogenes@gmail.com>
2019-08-19 17:45:54 -0300

    Subindo início do google maps

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:minha_bomba/tabs/home_tab.dart';
 import 'package:minha_bomba/widgets/custom_drawer.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class HomeMinhaBomba extends StatefulWidget {
   @override
@@ -11,16 +15,60 @@ class _HomeMinhaBombaState extends State<HomeMinhaBomba> {
 
   final _pageController = PageController();
 
+  var location = new Location();
+
+  Map<String, double> userLocation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _getLocation().then((value) {
+      setState(() {
+        userLocation = value;
+      });
+    });
+
+  }
+
+  CameraPosition _initialPosition = CameraPosition(target: LatLng(26.8206, 30.8025));
+  Completer<GoogleMapController> _controller = Completer();
+  MapType _defaultMapType = MapType.normal;
+
+  void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
+
   @override
   Widget build(BuildContext context) {
     return PageView(
+      physics: NeverScrollableScrollPhysics(),
       controller: _pageController,
       children: <Widget>[
         Scaffold(
-          body: HomeTab(),
+          appBar: AppBar(title: Text("Minha Bomba"), centerTitle: true,),
+          body: GoogleMap(
+            mapType: _defaultMapType,
+            myLocationEnabled: true,
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: null,
+          ),
           drawer: CustomDrawer(),
         )
       ],
     );
   }
+
+
+  Future<Map<String, double>> _getLocation() async {
+    var currentLocation = <String, double>{};
+    try {
+      currentLocation = await location.getLocation();
+    } catch (e) {
+      currentLocation = null;
+    }
+    return currentLocation;
+  }
+
 }
--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Widget _buildDrawerBack() =>
-        Container(
+    Widget _buildBodyBack() => Container(
           decoration: BoxDecoration(
               gradient: LinearGradient(
-                  colors: [
-                    Color.fromARGB(255, 203, 236, 241),
-                    Colors.white
-                  ],
+                  colors: [Color.fromARGB(255, 203, 236, 241), Colors.white],
                   begin: Alignment.topLeft,
-                  end: Alignment.bottomCenter
-              )
-          ),
+                  end: Alignment.bottomCenter)),
         );
+
+    return Stack(
+      children: <Widget>[
+        _buildBodyBack(),
+        Container(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: GoogleMap(
+                  onMapCreated: (GoogleMapController controller) {},
+                ),
+              ),
+              Container(
+                color: Colors.red,
+              ),
+              Container(color: Colors.blueAccent)
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
